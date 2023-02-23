8d3015269552fbd6ccc7c301d6d0c09f74936dbd
Damar Buana Murti <damarbm30@gmail.com>
2023-02-23 20:45:09 +0700

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { Login } from "./pages";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Routes>
+        <Route index path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
