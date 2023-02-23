bd0738fd8d3bd15327c4b20cb184572d4da2173e
Damar Buana Murti <damarbm30@gmail.com>
2023-02-23 16:07:01 +0700

    add assets

---
+++ src/assets/index.js
@@ -0,0 +1,25 @@
+import carVector from "./car-vector.png";
+import clock from "./clock.svg";
+import edit from "./edit.svg";
+import home from "./home.svg";
+import key from "./key.svg";
+import loginImg from "./login-img.png";
+import logo from "./logo.svg";
+import plus from "./plus.svg";
+import smallLogo from "./small-logo.svg";
+import trash from "./trash.svg";
+import truck from "./truck.svg";
+
+export {
+  carVector,
+  clock,
+  edit,
+  home,
+  key,
+  loginImg,
+  logo,
+  plus,
+  smallLogo,
+  trash,
+  truck,
+};
