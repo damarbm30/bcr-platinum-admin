2eef1f422ecd2f34eb45d1a939e0e44f3a5c7d47
Damar Buana Murti <damarbm30@gmail.com>
2023-02-25 19:59:41 +0700

    create new component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div>Logo</div>
+      <div>
+        <div>
+          <form>
+            <input type="text" />
+            <button>Search</button>
+          </form>
+          <div>
+            <div>Logo</div>
+            <p>Nama</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
