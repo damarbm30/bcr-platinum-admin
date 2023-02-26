6e2c6b7a0391ceaeedbe895c8bc3a9d17ca036f2
Damar Buana Murti <damarbm30@gmail.com>
2023-02-26 10:01:05 +0700

    get sidebar width

--- src/routes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes.jsx
@@ -1,13 +1,24 @@
 import { Outlet } from "react-router-dom";
+import { useRef, useLayoutEffect, useState } from "react";
+
 import { Navbar, Sidebar } from "../components";
 
 const ProtectedRoutes = () => {
+  const ref = useRef(null);
+  const [width, setWidth] = useState(0);
+
+  useLayoutEffect(() => {
+    setWidth(ref.current.offsetWidth);
+  }, []);
+
   return (
-    <>
-      <Navbar />
-      <Sidebar />
-      <Outlet />
-    </>
+    <div className="d-flex">
+      <Sidebar innerRef={ref} />
+      <div className="w-100">
+        <Navbar sidebarWidth={width} />
+        <Outlet />
+      </div>
+    </div>
   );
 };
 export default ProtectedRoutes;
