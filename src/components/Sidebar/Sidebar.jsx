8f931373a75fa675424f4d889852e86aeece6481
Damar Buana Murti <damarbm30@gmail.com>
2023-02-26 10:38:59 +0700

    add logo

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
+import { home, smallLogo, truck } from "../../assets";
 
 const Sidebar = ({ innerRef }) => {
-  const Sidebar = styled.div`
+  const Container = styled.div`
     background-color: var(--primaryBlue);
     height: 100vh;
     width: 70px;
@@ -9,11 +10,32 @@ const Sidebar = ({ innerRef }) => {
   `;
 
   return (
-    <Sidebar>
-      <div ref={innerRef} className="text-white">
-        Sidebar
+    <Container>
+      <div
+        ref={innerRef}
+        className="d-flex flex-column justify-content-center align-items-center p-2 gap-4"
+      >
+        <div>
+          <img src={smallLogo} alt="small-logo" />
+        </div>
+        <div className="d-flex flex-column align-items-center justify-content-center">
+          <button className="border-0 bg-transparent">
+            <img src={home} alt="home" />
+            <p className="text-white mb-0" style={{ fontSize: "0.75rem" }}>
+              Dashboard
+            </p>
+          </button>
+        </div>
+        <div className="d-flex flex-column align-items-center justify-content-center">
+          <button className="border-0 bg-transparent">
+            <img src={truck} alt="truck" />
+            <p className="text-white mb-0" style={{ fontSize: "0.75rem" }}>
+              Cars
+            </p>
+          </button>
+        </div>
       </div>
-    </Sidebar>
+    </Container>
   );
 };
 export default Sidebar;
