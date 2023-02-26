d726af3d88c5dbf0d90d6de50e4e770c8faa2574
Damar Buana Murti <damarbm30@gmail.com>
2023-02-26 10:00:58 +0700

    add Sidebar protoype

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,19 @@
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+import styled from "styled-components";
+
+const Sidebar = ({ innerRef }) => {
+  const Sidebar = styled.div`
+    background-color: var(--primaryBlue);
+    height: 100vh;
+    width: 70px;
+    z-index: 1050;
+  `;
+
+  return (
+    <Sidebar>
+      <div ref={innerRef} className="text-white">
+        Sidebar
+      </div>
+    </Sidebar>
+  );
 };
 export default Sidebar;
