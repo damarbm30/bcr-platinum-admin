e87c770fe6709fcdfe6936ae53bc66bda3810753
Damar Buana Murti <damarbm30@gmail.com>
2023-02-26 10:00:46 +0700

    fixed Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,32 @@
-const Navbar = () => {
+import { logo, menu } from "../../assets";
+
+const Navbar = ({ sidebarWidth }) => {
   return (
-    <nav>
-      <div>Logo</div>
-      <div>
-        <div>
-          <form>
-            <input type="text" />
-            <button>Search</button>
+    <nav className="navbar fixed-top bg-light" style={{ left: sidebarWidth }}>
+      <div className="container-fluid">
+        <div className="d-flex gap-5">
+          <button className="border-0 bg-transparent">
+            <img src={logo} alt="logo" />
+          </button>
+          <button className="border-0 bg-transparent">
+            <img src={menu} alt="menu" />
+          </button>
+        </div>
+        <div className="d-flex align-items-center gap-3">
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
           </form>
-          <div>
+          <div className="d-flex align-items-center gap-2">
             <div>Logo</div>
-            <p>Nama</p>
+            <div>Nama</div>
           </div>
         </div>
       </div>
