1dfb23d1826fabcbaea9b91fab604c513e377767
Damar Buana Murti <damarbm30@gmail.com>
2023-02-26 10:39:06 +0700

    add container

--- src/pages/Cars/Cars.jsx
+++ src/pages/Cars/Cars.jsx
@@ -1,4 +1,21 @@
+import styled from "styled-components";
+
 const Cars = () => {
-  return <div>Cars</div>;
+  const Container = styled.main`
+    position: relative;
+    width: 100%;
+    background-color: var(--background);
+  `;
+
+  return (
+    <Container>
+      <div className="position-absolute" style={{ top: "54px" }}>
+        <p>Cars</p>
+        <p>Cars</p>
+        <p>Cars</p>
+        <p>Cars</p>
+      </div>
+    </Container>
+  );
 };
 export default Cars;
