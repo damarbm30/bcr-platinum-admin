0f4aa1d7f86407e24bc4816518aaa216044dfd5c
Damar Buana Murti <damarbm30@gmail.com>
2023-02-28 20:12:19 +0700

    add header

--- src/pages/Cars/Cars.jsx
+++ src/pages/Cars/Cars.jsx
@@ -1,25 +1,60 @@
 import styled from "styled-components";
+import { plus } from "../../assets";
 import { InnerSidebar } from "../../components";
+import Breadcrumb from "./Breadcrumb";
 
 const Cars = () => {
-  const Container = styled.main`
+  const Content = styled.div`
     position: relative;
     width: 100%;
     background-color: var(--background);
   `;
 
+  const Button = styled.button`
+    background-color: var(--primaryBlue);
+    color: white;
+  `;
+
+  const CapacityButton = styled.button`
+    background-color: white;
+    color: var(--hoverBlue);
+    padding: 0.25em 0.5em;
+    border: 1px solid var(--hoverBlue);
+    border-radius: 4px;
+    font-weight: 500;
+    &:focus {
+      color: var(--primaryBlue);
+      border-color: var(--primaryBlue);
+      background-color: var(--hoverBlue);
+    }
+  `;
+
+  const PEOPLE_COUNT = ["All", "2 - 4 people", "4 - 6 people", "6 - 8 people"];
+
   return (
-    <>
+    <main className="d-flex w-100">
       <InnerSidebar />
-      <Container>
-        <div className="position-absolute" style={{ top: "54px" }}>
-          <p>Cars</p>
-          <p>Cars</p>
-          <p>Cars</p>
-          <p>Cars</p>
+      <Content>
+        <div className="position-absolute w-100" style={{ top: "54px" }}>
+          <div className="d-flex flex-column p-4 gap-4">
+            <Breadcrumb />
+            <div className="d-flex w-100 justify-content-between">
+              <h4 className="fw-bold">List Car</h4>
+              <Button className="btn btn-primary outline-none border-0 d-flex px-3 py-2 align-items-center gap-2">
+                <img src={plus} alt="plus" />
+                <p className="mb-0">Add New Car</p>
+              </Button>
+            </div>
+            <div className="d-flex gap-3">
+              {PEOPLE_COUNT.map((item) => {
+                return <CapacityButton>{item}</CapacityButton>;
+              })}
+            </div>
+            <p>Car List</p>
+          </div>
         </div>
-      </Container>
-    </>
+      </Content>
+    </main>
   );
 };
 export default Cars;
