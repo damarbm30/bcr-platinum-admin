77303650936268a241abb2a1cef7f25d319d0a88
Damar Buana Murti <damarbm30@gmail.com>
2023-03-01 09:34:04 +0700

    add login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,16 +8,15 @@ import { ProtectedRoutes, PublicRoutes } from "./routes";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route element={<PublicRoutes />}>
-          <Route index path="/login" element={<Login />} />
-        </Route>
-        <Route element={<ProtectedRoutes />}>
-          <Route index path="/cars" element={<Cars />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route element={<PublicRoutes />}>
+        <Route index path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Route>
+      <Route element={<ProtectedRoutes />}>
+        <Route index path="/cars" element={<Cars />} />
+      </Route>
+    </Routes>
   );
 }
 
