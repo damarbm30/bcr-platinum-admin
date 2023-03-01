a35e86eae3d68ecb1c445ab07809b658bc672a48
Damar Buana Murti <damarbm30@gmail.com>
2023-03-01 09:35:03 +0700

    add navigate after login

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,11 +1,12 @@
 import api from "./baseUrl";
 
-export const adminLogin = async (data, setIsError) => {
+export const adminLogin = async (data, setIsError, navigate) => {
   try {
     const result = await api.post("/admin/auth/login", data);
     const accessToken = JSON.stringify(result.data.access_token);
     localStorage.setItem("adminCredential", accessToken);
     setIsError(false);
+    navigate("/cars");
   } catch (error) {
     console.log(error);
     setIsError(true);
