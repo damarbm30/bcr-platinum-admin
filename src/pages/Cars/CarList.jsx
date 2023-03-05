68d68cadad60f5b36a5cc0433aa38da1b68ace23
Damar Buana Murti <damarbm30@gmail.com>
2023-03-05 17:57:32 +0700

    implement filter by category

--- src/pages/Cars/CarList.jsx
+++ src/pages/Cars/CarList.jsx
@@ -4,9 +4,28 @@ import { getCars } from "../../services/carServices";
 import useCar from "../../store/carList";
 import CarItem from "./CarItem";
 
-const CarList = () => {
+const CarList = ({ active }) => {
   const carList = useCar((state) => state.carList);
   const setCarList = useCar((state) => state.setCarList);
+
+  let peopleCap;
+
+  switch (active.toLowerCase()) {
+    case "2 - 4 people":
+      peopleCap = "small";
+      break;
+    case "4 - 6 people":
+      peopleCap = "medium";
+      break;
+    case "6 - 8 people":
+      peopleCap = "large";
+      break;
+  }
+
+  const filteredCarList =
+    active !== "All"
+      ? carList.filter((car) => car.category.toLowerCase() === peopleCap)
+      : carList;
 
   useEffect(() => {
     async function getCarsAsync() {
@@ -23,7 +42,7 @@ const CarList = () => {
   return (
     <div className="container-fluid">
       <div className="row gap-5">
-        {carList?.map((car) => {
+        {filteredCarList?.map((car) => {
           const { id, image, name, price, category, updatedAt } = car;
           return (
             <CarItem
