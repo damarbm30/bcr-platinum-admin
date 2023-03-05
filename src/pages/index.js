1b3e6081c67ec0b3b4fd8f251fe15615dd7d6891
Damar Buana Murti <damarbm30@gmail.com>
2023-03-05 17:56:40 +0700

    update indexing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import Login from "./Login";
 import Cars from "./Cars";
+import NewCar from "./NewCar";
+import EditCar from "./EditCar";
 
-export { Login, Cars };
+export { Login, Cars, NewCar, EditCar };
