e0bc2057ece03a8be1410505d521a07c9f22c242
Damar Buana Murti <damarbm30@gmail.com>
2023-03-06 21:51:02 +0700

    add Dashboard

--- src/components/Breadcrumb/Breadcrumb.jsx
+++ src/components/Breadcrumb/Breadcrumb.jsx
@@ -5,16 +5,36 @@ const Wrapper = styled.div`
   --bs-breadcrumb-divider: url(${chevronRight});
 `;
 
-const Breadcrumb = ({ newCar, editCar }) => {
+const Breadcrumb = ({ dashboard, cars, newCar, editCar }) => {
   return (
     <Wrapper className="d-flex">
       <ul className="breadcrumb">
-        <li className="breadcrumb-item fw-bold">Cars</li>
-        <li className={`breadcrumb-item ${newCar || editCar ? "fw-bold" : ""}`}>
-          List Car
-        </li>
-        {newCar ? <li className="breadcrumb-item">Add New Car</li> : ""}
-        {editCar ? <li className="breadcrumb-item">Edit Car</li> : ""}
+        {cars && (
+          <>
+            <li className="breadcrumb-item fw-bold">Cars</li>
+            <li
+              className={`breadcrumb-item ${
+                newCar || editCar ? "fw-bold" : ""
+              }`}
+            >
+              List Car
+            </li>
+            {newCar ? <li className="breadcrumb-item">Add New Car</li> : ""}
+            {editCar ? <li className="breadcrumb-item">Edit Car</li> : ""}
+          </>
+        )}{" "}
+        {dashboard && (
+          <>
+            <li className="breadcrumb-item fw-bold">Dashboard</li>
+            <li
+              className={`breadcrumb-item ${
+                newCar || editCar ? "fw-bold" : ""
+              }`}
+            >
+              Dashboard
+            </li>
+          </>
+        )}
       </ul>
     </Wrapper>
   );
