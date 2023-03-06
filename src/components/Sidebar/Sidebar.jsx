45223df31ceb372398cb5a536af78a68ad185762
Damar Buana Murti <damarbm30@gmail.com>
2023-03-06 21:51:13 +0700

    add Dashboard

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 import { home, smallLogo, truck } from "../../assets";
@@ -17,37 +17,37 @@ const Sidebar = ({ innerRef }) => {
       <div
         ref={innerRef}
         className="d-flex flex-column justify-content-center align-items-center w-100 pt-2"
-        // style={{ gap: "1.1rem" }}
       >
         <div>
           <img src={smallLogo} alt="small-logo" />
         </div>
-        {/* <div className="d-flex flex-column align-items-center justify-content-center">
-          <button className="border-0 bg-transparent">
-            <img src={home} alt="home" />
-            <p className="text-white mb-0" style={{ fontSize: "0.75rem" }}>
-              Dashboard
-            </p>
-          </button>
-        </div> */}
         <div className="d-flex w-100 flex-column align-items-center justify-content-center mt-3">
-          <div
+          <NavLink
+            to="/dashboard"
             className="w-100 py-1"
-            // style={{ backgroundColor: "rgba(255,255,255,0.3)" }}
+            style={({ isActive }) =>
+              isActive
+                ? { backgroundColor: "rgba(255,255,255,0.3)" }
+                : undefined
+            }
           >
             <button className="border-0 p-2 bg-transparent w-100">
-              <img src={home} alt="home" />
+              <img src={home} alt="truck" />
               <p className="text-white mb-0" style={{ fontSize: "0.75rem" }}>
                 Dashboard
               </p>
             </button>
-          </div>
+          </NavLink>
         </div>
         <div className="d-flex w-100 flex-column align-items-center justify-content-center">
-          <Link
+          <NavLink
             to="/cars"
             className="w-100 py-1"
-            style={{ backgroundColor: "rgba(255,255,255,0.3)" }}
+            style={({ isActive }) =>
+              isActive
+                ? { backgroundColor: "rgba(255,255,255,0.3)" }
+                : undefined
+            }
           >
             <button className="border-0 p-2 bg-transparent w-100">
               <img src={truck} alt="truck" />
@@ -55,7 +55,7 @@ const Sidebar = ({ innerRef }) => {
                 Cars
               </p>
             </button>
-          </Link>
+          </NavLink>
         </div>
       </div>
     </Container>
