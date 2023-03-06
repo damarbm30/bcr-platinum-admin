a3efb857864b6729b7ca240d934e97b782d59809
Damar Buana Murti <damarbm30@gmail.com>
2023-03-06 17:24:33 +0700

    get car id

--- src/pages/Cars/CarList.jsx
+++ src/pages/Cars/CarList.jsx
@@ -1,10 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
 
-import { getCars } from "../../services/carServices";
+import "react-toastify/dist/ReactToastify.css";
+import { getCars, deleteCar } from "../../services/carServices";
 import useCar from "../../store/carList";
 import CarItem from "./CarItem";
+import DeleteModal from "./DeleteModal";
 
 const CarList = ({ active }) => {
+  const [carId, setCarId] = useState(null);
+  const [isSuccess, setIsSuccess] = useState({
+    add: false,
+    edit: false,
+    delete: false,
+  });
+
   const carList = useCar((state) => state.carList);
   const setCarList = useCar((state) => state.setCarList);
 
@@ -36,12 +46,38 @@ const CarList = ({ active }) => {
       });
     }
 
+    // if (isSuccess.add || isSuccess.edit || isSuccess.delete) {
     getCarsAsync();
-  }, []);
+    // }
+  }, [isSuccess.add, isSuccess.edit, isSuccess.delete]);
+
+  const handleDelete = async (carId) => {
+    setIsSuccess({ ...isSuccess, delete: false });
+    const result = await deleteCar(carId);
+
+    if (result.status === 200) {
+      setIsSuccess({ ...isSuccess, delete: true });
+      toast("Data Berhasil Dihapus", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: false,
+        progress: undefined,
+        theme: "dark",
+        className: "text-center",
+      });
+    }
+  };
+
+  const handleGetId = (id) => {
+    setCarId(id);
+  };
 
   return (
     <div className="container-fluid">
-      <div className="row gap-5">
+      <div className="row gap-5 justify-content-center">
         {filteredCarList?.map((car) => {
           const { id, image, name, price, category, updatedAt } = car;
           return (
@@ -53,10 +89,13 @@ const CarList = ({ active }) => {
               price={price}
               category={category}
               updatedAt={updatedAt}
+              onGetId={handleGetId}
             />
           );
         })}
       </div>
+      <DeleteModal carId={carId} onDelete={handleDelete} />
+      <ToastContainer closeButton={false} />
     </div>
   );
 };
